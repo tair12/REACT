1901ff0e69d1f8e38a67dcb0e167ba3d62a269bc
tair12 <tair-00@list.ru>
2022-07-20 13:21:25 +0300

    dz2

--- src/App.js
+++ src/App.js
@@ -16,14 +16,7 @@ import React from 'react';
 
 function UsersList(props) {
     return (
-        <ul>
-            <li>{props.user.name} - {props.user.age}</li>
-            {props.usersName.map(user => {
-                return (
-                    <li>{user}</li>
-                )
-            })}
-        </ul>
+         <div> Hello world </div>
     );
 }
 
