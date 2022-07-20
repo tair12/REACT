a691ae5365296d8334d00496a37e5da94343bbfd
tair12 <tair-00@list.ru>
2022-07-20 12:34:56 +0300

    lesson1

--- src/App.js
+++ src/App.js
@@ -1,21 +1,32 @@
-function AboutUs ( props){
-   return(
-          <div>
-            <h1>
-               AboutUs
-            </h1>
-          </div>
-   )
+
+// import Header from "./components/header/Header";
+// function App(){
+//    return(
+//      <div className = "container"> 
+//       <Header/>
+//      </div>
+//    );
+// }
+// export default App;
+
+
+
+
+import React from 'react';
+
+function UsersList(props) {
+    return (
+        <ul>
+            <li>{props.user.name} - {props.user.age}</li>
+            {props.usersName.map(user => {
+                return (
+                    <li>{user}</li>
+                )
+            })}
+        </ul>
+    );
 }
 
+export default UsersList;
 
 
-
-function App(){
-   return(
-     <div className = "container"> 
-     <AboutUs title=" Git"/>
-     </div>
-   );
-}
-export default App;
---
+++ src/components/header/Header.js
@@ -0,0 +1,13 @@
+// import React from "react";
+
+
+// function(){
+//    return(
+//       <ul>
+//          <li> Main page </li>
+//          <li> About  page </li>
+//          <li> contact page </li>
+//       </ul>
+//    )
+// }
+//  export default Header;
